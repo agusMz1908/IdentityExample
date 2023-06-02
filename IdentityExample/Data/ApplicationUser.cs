c24a7fd131b18ca5f5eda9b082c7f0e989a01dcb
Agustin Muniz <agus.muniz1997@gmail.com>
2023-06-01 21:00:48 -0300

    Identity Example

---
+++ IdentityExample/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityExample.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string? SecondName { get; set; }
+    }
+
+}
---
+++ IdentityExample/Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdentityExample.Data
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int Code { get; set; }
+    }
+}
